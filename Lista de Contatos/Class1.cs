2df0eee709641d329a3565a6f5e6f61acecc1c99
calvinho43 <guibrittotava@gmail.com>
2024-04-25 15:23:48 -0300

    Saída dos dados do contato

---
+++ Lista de Contatos/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Contatos
+{
+    internal class ClassContato
+    {
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Sobrenome
+        {
+            get
+            {
+                return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(43) 43434-4343";
+                }
+            }
+        }
+        public ClassContato()
+        {
+            Nome = "Calvo";
+            Sobrenome = "Calvinho";
+            Telefone = "(43) 43434-4343";
+        }
+
+        public ClassContato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            string saida = string.Empty;
+            saida += String.Format("{0} {1}", Nome, Sobrenome);
+            saida += String.Format("({0}) {1}-{2}",
+                Telefone.Substring(0,2), Telefone.Substring(2,5), Telefone.Substring(7, 4));
+            return saida;
+        }
+
+    }
+}
